0ed22c6e7e9328ec8320acd9afb730545ad19949
wzk <wzk774583624@gmail.com>
2020-11-08 13:21:29 +0800

    111

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,15 +8,20 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { PapaParseModule } from 'ngx-papaparse';
+import { File } from '@ionic-native/file/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,HttpClientModule,
+    PapaParseModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    File
   ],
   bootstrap: [AppComponent]
 })
--- src/app/scorecard/scorecard.page.ts
+++ src/app/scorecard/scorecard.page.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Papa } from 'ngx-papaparse';
+import { Platform } from '@ionic/angular';
+import { File } from '@ionic-native/file/ngx';
 
 @Component({
   selector: 'app-scorecard',
@@ -7,12 +11,69 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScorecardPage implements OnInit {
 
-  categories = ['Financial', 'Operations', 'Digitalisation', 
-  'Customer', 'People', 'Products'];
+  categories = ['Financial', 'Operations', 'Digitalisation',
+    'Customer', 'People', 'Products'];
 
-  constructor() { }
+  RevenueData: any[] = [];
+  RevenueKPI: any[] = [];
+  FinancialFirstType
+  Financial: any[] = [];
+  FinancialType: any[] = [];
+  FinancialTotal: any[] = [];
+  headerRow: any[] = [];
+
+  constructor(private http: HttpClient,
+    private papa: Papa,
+    private plt: Platform,
+    private file: File) {
+    this.Revenue();
+    this.Target();
+  }
 
   ngOnInit() {
   }
 
+  private Revenue(){
+    this.http.get('../../assets/Revenue.csv', {
+      responseType: 'text'
+    })
+    .subscribe(
+      data => this.extractRevenue(data),
+      err => console.log('something went wrong: ', err)
+    );
+  }
+  private Target(){
+    this.http.get('../../assets/Target.csv', {
+      responseType: 'text'
+    })
+    .subscribe(
+      data => this.extractTarget(data),
+      err => console.log('something went wrong: ', err)
+    );
+  }
+
+  private extractTarget(res) {
+    let Financial = res || '';
+ 
+    this.papa.parse(Financial, {
+      complete: parsedData => {
+        // this.headerRow = parsedData.data.splice(0, 1)[0];
+        this.FinancialFirstType = parsedData.data.splice(5,1);
+        this.Financial = parsedData.data.splice(5,8);
+        this.FinancialType = parsedData.data.splice(10,1);
+      }
+    });
+  }
+  private extractRevenue(res) {
+    let RevenueData = res || '';
+ 
+    this.papa.parse(RevenueData, {
+      complete: parsedData => {
+        // this.headerRow = parsedData.data.splice(0, 1)[0];
+        this.RevenueKPI = parsedData.data.splice(0,1);
+        this.RevenueData = parsedData.data.splice(15,1);
+        // this.RevenueData = parsedData.data.splice(0,10)[0];
+      }
+    });
+  }
 }
